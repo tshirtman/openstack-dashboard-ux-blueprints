06dbf3b7db7b9f7af9035f7fde06acb3d9a5324d
Gabriel Pettier <gabriel.pettier@cloudwatt.com>
2014-01-30 12:05:43 +0100

    trying to use controller to get values in the directive

--- launch-instance/js/horizon-ux.js
+++ launch-instance/js/horizon-ux.js
@@ -4,6 +4,9 @@ angular.module('horizon-ux', []).directive('hzSelect', function(){
     link: function($scope, element, attribute){
       console.log('it works!', $scope.name);
     },
+    controller: function($scope){
+      $scope.$watch('foo', function () {console.log($scope.foo)});
+    },
     scope: {
       'name': '@'
     }
